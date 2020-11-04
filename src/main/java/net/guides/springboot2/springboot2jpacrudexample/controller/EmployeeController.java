357bc55e7ac64a7ded103b4840bfedfd8b2e8279
Mahm0udSaed <mahmoudsaltah145@gmail.com>
2020-11-04 12:24:53 +0200

    Add initial Jenkinsfile

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/EmployeeController.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/EmployeeController.java
@@ -23,7 +23,7 @@ import net.guides.springboot2.springboot2jpacrudexample.exception.ResourceNotFou
 import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
 import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://dummy.restapiexample.com")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
